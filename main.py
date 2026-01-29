b62a44d1026541fb170a04b0e2e8fac84a310e3e
KBanaag-10E <banaag.kj@obmontessori.edu.ph>
2026-01-29 08:37:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from pyscript import display, document
 
 def check_requirements(e):
 
-    registration = document.querySelector("input[name='yes1']:checked").value
-    medical_clearance = document.querySelector("input[name='yes2']:checked").value
+    registration = document.querySelector("input[name='reg']:checked").value
+    medical_clearance = document.querySelector("input[name='med']:checked").value
     grade_level = document.getElementById("grade-level").value
     section = document.getElementById("section").value
 
@@ -11,22 +11,22 @@ def check_requirements(e):
     document.getElementById("team_photo").innerHTML = ' '
 
     # Emerald
-    if section == "e": # & registration == "yes1" & medical_clearance == "yes2":
+    if grade_level == "g7" | grade_level == "g8" | grade_level == "g9" |  grade_level == "g10" & section == "e" & registration == "yes" & medical_clearance == "yes":
         display(f"You are part of the", target='output')
         document.getElementById("team_photo").innerHTML = "<img src='green_hornets.jpeg'>"
 
     # Ruby
-    elif section == "r": # & registration == "yes1" & medical_clearance == "yes2":
+    elif grade_level == "g7" | grade_level == "g8" | grade_level == "g9" |  grade_level == "g10" & section == "r" & registration == "yes" & medical_clearance == "yes":
         display(f"You are part of the", target='output')
         document.getElementById("team_photo").innerHTML = "<img src='red_bulldogs.jpeg'>"
 
     # Sapphire
-    elif section == "s": # & registration == "yes1" & medical_clearance == "yes2":
+    elif grade_level == "g7" | grade_level == "g8" | grade_level == "g9" |  grade_level == "g10" & section == "s" & registration == "yes" & medical_clearance == "yes":
         display(f"You are part of the", target='output')
         document.getElementById("team_photo").innerHTML = "<img src='blue_bears.jpeg'>"
     
     # Topaz
-    elif section == "t": # & registration == "yes1" & medical_clearance == "yes2":
+    elif grade_level == "g7" | grade_level == "g8" | grade_level == "g9" |  grade_level == "g10" & section == "t" & registration == "yes" & medical_clearance == "yes":
         display(f"You are part of the", target='output')
         document.getElementById("team_photo").innerHTML = "<img src='yellow_tigers.jpeg'>"
 
